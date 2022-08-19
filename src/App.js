9d8673ef6875483359f0117f9bde3742ab380d73
wasline <sf.wasline@gmail.com>
2022-08-18 21:38:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Form from "./Form";
+import Search from "./Search";
 
-function App() {
+import "./App.css";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div id="root">
+        <div className="card" id="first">
+          <div className="card-body">
+            <Form />
+            <Search />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
