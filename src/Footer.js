b59f48a9c139ccfa87f8caf93fb21ab1be4b6271
wasline <sf.wasline@gmail.com>
2022-08-21 14:48:42 -0400

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,12 +5,19 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div>
-        <footer className="footer">
-        This project was coded by Wasline Saint fleur and is{" "}
-            <a href="https://github.com/Wasline/React" target="_blank" rel="noreferrer">
-                open-sourced on GitHub
-            </a>
-        </footer>;
+      <footer className="footer">
+        <p>
+          This project was coded by Wasline Saint fleur and is{" "}
+          <a
+            href="https://github.com/Wasline/React"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </p>
+      </footer>
+      ;
     </div>
   );
 }
