f69ac74eb93acaa0179fa6239795aeb6ed0827b1
wasline <sf.wasline@gmail.com>
2022-08-21 14:40:21 -0400

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div>
+        <footer className="footer">
+        This project was coded by Wasline Saint fleur and is{" "}
+            <a href="https://github.com/Wasline/React" target="_blank" rel="noreferrer">
+                open-sourced on GitHub
+            </a>
+        </footer>;
+    </div>
+  );
+}
