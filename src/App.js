e040585b4016d8cadbcfe0d6d5bcdb8e2acae43b
wasline <sf.wasline@gmail.com>
2022-09-01 19:55:35 -0400

    Added className "App" to the div

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from "react";
 import Weather from "./Weather";
 import Footer from "./Footer";
 
-
 import "./App.css";
 
 export default function App() {
   return (
+   <div className="App">
     <div className="container mt-5">
       <div id="root">
         <div className="card" id="first">
           <div className="card-body">
-            <Weather defaultCity="San Fransico" />
-
+            <Weather defaultCity="San Francisco" />
           </div>
         </div>
       </div>
@@ -20,5 +19,6 @@ export default function App() {
         <Footer />
       </small>
     </div>
+   </div> 
   );
 }
