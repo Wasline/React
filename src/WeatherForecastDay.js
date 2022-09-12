9b2a2edf4369693d179638b0e5c5972d20b74e24
wasline <sf.wasline@gmail.com>
2022-09-12 00:41:29 -0400

    Add WeatherIcon

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,6 +1,12 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
+function weatherIcons(){
+  let icon =`.icons/weatherIcons{props.data.weather[0].icon}`
+  return icon;
+}
+
   function maxTemperature() {
     let temperature = Math.round(props.data.temp.max);
     return `${temperature}°`;
@@ -12,7 +18,7 @@ export default function WeatherForecastDay(props) {
   }
 
   function day(){
-    let date= new Date(props.data.dt*1000);
+    let date= new Date(props.data.dt * 1000);
     let day = date.getDay();
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
@@ -21,21 +27,20 @@ export default function WeatherForecastDay(props) {
 
   return (
     <div>
-      <div className="WeatherForecast-day">{props.data.dt}</div>
+      <div className="WeatherForecast-day">{day()}</div>
       <div className="WeatherIcon">
-        <img
-          src={props.data.weather[0].icon}
+        <WeatherIcon code ={props.data.waether[0].icon}
           className="img-fluid weather-icon"
           alt="weather-icon"
-          size={5}
+          size={10}
         />
       </div>
       <div className="WeatherForecast-temperature">
         <span className="WeatherForecast-temperature-max">
-          {maxTemperature()}°
+          {maxTemperature()}
         </span>
         <span className="WeatherForecast-temperature-min">
-          {minTemperature()}°
+          {minTemperature()}
         </span>
       </div>
     </div>
