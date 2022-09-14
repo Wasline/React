c5933d49a8a7a45edc5baeb682ef151355481a4c
wasline <sf.wasline@gmail.com>
2022-09-13 21:54:45 -0400

    Add missing weather icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -3,12 +3,20 @@ import clearDay from "./images/01d.svg";
 import clearNight from "./images/01n.svg";
 import overcastDay from "./images/02d.svg";
 import overcastNight from "./images/02n.svg";
-import cloudy from "./images/03d.svg";
-import overcast from "./images/04d.svg";
+import cloudyDay from "./images/03d.svg";
+import cloudyNight from "./images/03n.svg";
+import brokenCloudsDay from "./images/04d.svg";
+import brokenCloudsNight from "./images/04n.svg";
 import rain from "./images/09d.svg";
-import thunderstorms from "./images/11n.svg";
-import snow from "./images/13d.svg";
-import mist from "./images/50n.svg";
+import rainy from "./images/09n.svg";
+import rains from "./images/10d.svg";
+import rainn from "./images/10n.svg";
+import thunderstormsDay from "./images/11d.svg";
+import thunderstormsNight from "./images/11n.svg";
+import snowDay from "./images/13d.svg";
+import snowNight from "./images/13n.svg";
+import mistDay from "./images/50d.svg";
+import mistNight from "./images/50n.svg";
 
 export default function WeatherIcon(props) {
     const codeMapping = {
@@ -16,19 +24,20 @@ export default function WeatherIcon(props) {
       "01n": clearNight,
       "02d": overcastDay,
       "02n": overcastNight,
-      "03d": cloudy,
-      "03n": overcast,
-      "04n": overcast,
+      "03d": cloudyDay,
+      "03n": cloudyNight,
+      "04d": brokenCloudsDay,
+      "04n": brokenCloudsNight,
       "09d": rain,
-      "09n": rain,
-      "10d": rain,
-      "10n": rain,
-      "11d": thunderstorms,
-      "11n": thunderstorms,
-      "13d": snow,
-      "13n": snow,
-      "50d": mist,
-      "50n": mist,
+      "09n": rainy,
+      "10d": rains,
+      "10n": rainn,
+      "11d": thunderstormsDay,
+      "11n": thunderstormsNight,
+      "13d": snowDay,
+      "13n": snowNight,
+      "50d": mistDay,
+      "50n": mistNight,
     };
 
     return (
